3491c02c47ca8a45e44ca95c213d51f82cee1106
kirby@puppetlabs.com <mk.kirby@gmail.com>
2018-11-14 11:14:07 -0800

    Specify requires for packages for git and locales

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 class qe {
 
+  include git
   include stdlib
+  include qe::packages
 
   group {
     'docker':
@@ -20,19 +22,21 @@ class qe {
   }
 
   file_line { 'default_locale':
-    ensure => present,
-    path   => '/etc/locale.gen',
-    line   => 'en_US.UTF-8 UTF-8',
-    match  => '# en_US.UTF-8 UTF-8'
+    ensure  => present,
+    path    => '/etc/locale.gen',
+    line    => 'en_US.UTF-8 UTF-8',
+    match   => '# en_US.UTF-8 UTF-8',
+    require => Package['locales']
   }
 
-  exec { '/usr/sbin/locale-gen en_US.UTF-8': }
-
-  include git
+  exec { '/usr/sbin/locale-gen en_US.UTF-8':
+    require => File_line['default_locale']
+  }
 
   git::config {
     default:
-      user => 'jenkins'
+      user    => 'jenkins',
+      require => Package['git']
       ;
     'user.name':
       value => 'Jenkins CI'
